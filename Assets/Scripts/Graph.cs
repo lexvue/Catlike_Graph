5115128676fc960ff58e73e63e7b64707b1fd48d
lexvue <alexandertvue@gmail.com>
2024-04-30 13:55:10 -0500

    Finished implementing the graph. Animated sine of X, and with shading to pair with it

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -10,21 +10,36 @@ public class Graph : MonoBehaviour {
     [SerializeField, Range(10, 100)]
     int resolution = 10;
 
+    Transform[] points;
+
     void Awake() {
 
         //The Vector3 struct has three floating-point fields: x, y, and z. These fields are public, so we can change them.
         float step = 2f / resolution;
         Vector3 position = Vector3.zero; //Assigning all x, y, z of position to 0 so as to not err as an unassigned var
         var scale = Vector3.one * step;
-        for (int i = 0; i < resolution; i++) {
-            Transform point = Instantiate(pointPrefab); //creates a clone of pointPrefab into scen
+        points = new Transform[resolution];
+        
+        for (int i = 0; i < points.Length; i++) {
+            Transform point = points[i] = Instantiate(pointPrefab); //creates a clone of pointPrefab into scen
             //point.localPosition = Vector3.right * ((i + 0.5f) / 5f - 1f); //transforms the new clone by 1 * i on the X axis
             position.x = ((i + 0.5f) * step - 1f); // we no longer have to multiply by Vector3.right
-            position.y = position.x * position.x;
+            //position.y = position.x * position.x * position.x; //setting Y in Update
             point.localPosition = position;
             pointPrefab.localScale = scale; //reducing the scale of clones to prevent overlap
             point.SetParent(transform, false); //So all the cloned Points don't clutter the hierarchy
         }
 
+    }
+
+    void Update() {
+        float time = Time.time;
+        for (int i=0; i < points.Length; i++) {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = position.x * position.x * position.x;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time)); //Scaling the sine of X by pi to see the sine in its entirity
+            point.localPosition = position; //loading position back into localposition 
+        }
     }
 }
